68f9f554c012269106832fac4f6fa98cafa85719
James Ejembi <jamesejembi@users.noreply.github.com>
2026-06-04 19:36:11 +0100

    chore: refactor organization and repository naming references

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('SoroSusu API is running'));
+app.get('/', (req, res) => res.send('VeriNode API is running'));
 app.listen(3000, () => console.log('Server running on port 3000'));
